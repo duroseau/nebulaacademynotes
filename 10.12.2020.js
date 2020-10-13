f86038f10c8a0ad2ce683b1a456cfeb6818e7fb6
eddy duroseau <eddyduroseau@gmail.com>
2020-10-12 22:47:50 -0400

    10.12.2020.js

---
+++ 10.12.2020.js
@@ -0,0 +1,30 @@
+let name='EddyDuroseau';
+console.log(name)
+let level0='WeAreTheWorld';
+console.log(level0)
+let level1='WE_ARE_SHOUTING!';
+console.log(level1)
+console.log(level1.length)
+console.log
+let level2='HelloWorld';
+console.log(level2[6])
+let level3='WelcomeWorld!'
+console.log
+
+
+
+
+console.log
+console.log
+console.log(level3.replace("!"/"?"))
+console.log(level3.replace("!","?"))
+console.log
+console.log(level2[0],[9])
+console.log(level3[0])
+console.log(level3[11])
+let level4='WelcomeToMyWebsite';
+console.log(level4[6])
+console.log
+console.log(level4[6])
+console.log
+
