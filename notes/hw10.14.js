f0458e7328ec0ea405603e92da018947b2e72f83
eddy duroseau <72086145+duroseau@users.noreply.github.com>
2020-10-16 15:09:15 -0400

    Update hw10.14.js

--- notes/hw10.14.js
+++ notes/hw10.14.js
@@ -14,6 +14,19 @@ console.log(brooklyn.slice(7, 15))
 
 console.log
 console.log(brooklyn.slice(36, 43))
+let brownsville='we are learning new strings'
+console.log(brownsville)
+console.log(brownsville.length)
+console.log(brownsville.slice('-1'))
+console.log(brownsville.charAt(5))
+console.log(brownsville.replace('a','-'))
+console.log(brownsville.toUpperCase())
+console.log(brownsville.toLowerCase())
+let PistachioAlmond=`My favorite ice cream flavor is pistachio almond`
+console.log(PistachioAlmond)
+console.log
+console.log
+console.log
 
 
 
