a106945eb082ddeed787d660a13710dba0ebda9b
eddy duroseau <eddyduroseau@gmail.com>
2020-10-20 22:23:50 -0400

    1019hw2.js

--- notes/wed10-14.js
+++
@@ -1,9 +0,0 @@
-let name='eddy duroseau'
-let fullName=name.split('')
-console.log(fullName)
-let firstName=fullName[0]
-let lastName=fullName[1]
-console.log(firstName[0])
-console.log(lastName[0])
-console.log(firstName)
-console.log(lastName)
