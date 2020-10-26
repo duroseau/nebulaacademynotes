94cec7ef906810ef0da5847a1c6f5ab75bf6a5c9
eddy duroseau <eddyduroseau@gmail.com>
2020-10-26 11:56:13 -0400

    10.24.js

---
+++ notes/10.24.js
@@ -0,0 +1,45 @@
+//function fullName(firstName, lastName){
+  //  console.log(firstName)
+   // console.log(lastName)
+   // return "je m'appelle " + firstName + lastName}
+//console.log(fullName("Eddy", "Duroseau"))//=>My name is Eddy Duroseau (in French)
+//function myDebt(bill1, bill2){
+  //  return bill1 + bill2}
+  //  console.log(myDebt(150, 189))//my debt is 339
+ //function solution(num, greet) {
+   //  return solution}  //  
+   // console.log(num = 400)//
+   // console.log(greet = 'hi')//=>solution =>400 'hi'
+   //let fruits = ['apple',' orange', 'peach', 'lemon','plum', 'grape', 'watermelon']//
+//function purplefruit (individualfruit){
+   // return `the purple fruit is ${individualfruit}`//=>the purple fruit is plum
+//}//
+//console.log(fruits.pop())//=>watermelon
+//console.log(purplefruit(fruits[4]))//=>the purple fruit is plum
+   //   console.log(fruits.slice(2,6))//=>peach,lemon.plum,grape
+   //console.log(fruits.push ('banana'))//=>8 adds new element to array
+//console.log(fruits)//=>banana at the end of array
+//console.log(fruits.join(' and '))//=> added and after each element in array
+let names = ['Jasmine Maurice Chelsea Anitria Tauseef Daja EDDY']
+console.log(names)
+function nebulaAcademy (classMates){
+    return 'Hello ' +  classMates
+}
+//console.log(nebulaAcademy('Maurice and Chelsea'))//=> Hello Maurice and Chelsea
+//function nebulaAcademy (classMates){
+   // return "hi " + classMates
+
+   // console.log(nebulaAcademy('Jasmine and Daja'))//=> Hi Jasmine and Daja
+
+//function nebulaAcademy (classMates){
+    //return 'Howdy ' + classMates
+//}
+//console.log(nebulaAcademy('Eddy'))//=> Howdy Eddy
+//function nebulaAcademy (classMates){
+    //return 'Happy Monday ' + classMates
+//}
+   // console.log(nebulaAcademy('Anitria'))//=> Happy Monday Anitria
+   //function nebulaAcademy (classMates){
+       //return 'Hey ' + classMates
+  // }
+    //console.log(nebulaAcademy('Tauseef'))//=> Hey Tauseef
---
+++ notes/newFile.js
@@ -0,0 +1,13 @@
+let fruits = ['apples, oranges, grapes, lemon, watermelon']
+
+function twoFruits(fruitArray){
+    console.log(fruitArray)
+    
+    return 'hello ' + fruitArray;
+}
+    console.log(twoFruits('apples, oranges'))
+    {
+        return `{twoFruits}`
+    }
+    
+function 
