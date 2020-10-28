489e54c27f25573478a43678f38a265423592d37
eddy duroseau <eddyduroseau@gmail.com>
2020-10-28 10:22:20 -0400

    10.24hw.js

---
+++ notes/10.24hw.js
@@ -0,0 +1,56 @@
+// let zoey = "Eddy Duroseau"
+// function offGuard (speedy){
+//     return speedy
+// }
+// console.log(offGuard(zoey))
+
+// function adding(light1, gas1){
+//     return light1 + gas1
+// }
+// console.log(adding(150, 175))
+
+// let pro3 = [22, 18, "Eddy", "zoey", "Hi", 400]
+//     function backup (pays){
+//     return pays.splice(4, 5)
+// }
+// console.log(backup(pro3))
+
+var fruits = ['apple', 'orange', 'peach', 'lemon', 'plum', 'grape', 'watermelon']
+ //function fourth (date){
+//     return 'the fourth element is'
+// }
+// console.log(fourth(fruits[4]))
+
+// return fruits[fruits.length-1]
+//  }
+//  console.log(fourth(fruits))
+// function fourFruit (four){
+//     return four.splice(2, 4)
+// }
+// console.log(fourFruit(fruits))
+// function plus (yellowFruit){
+//     return yellowFruit.push('banana')
+// }
+// console.log(plus(fruits))
+// console.log(fruits)
+// function and1 (fruits){
+//     return fruits.join(' and ')
+// }
+// console.log(and1(fruits))
+
+var names = ['Jasmine', 'Maurice', 'Chelsea', 'Anitria', 'Tauseef', 'Daja', 'Eddy']
+function nebulaAcademy (students){
+//     return "hello " + students[1] + ' and ' + students[2]
+// }
+// console.log(nebulaAcademy(names))
+//     return 'Hi ' + students[0] + ' and ' + students[5]
+// }
+// console.log(nebulaAcademy(names))
+//     return "Howdy " + students[6]
+// }
+// console.log(nebulaAcademy(names))
+//     return "Happy Monday " + students[3]
+// }
+// console.log(nebulaAcademy(names))
+//
+Teacher: I had to Redo this, wish me luck with tonight's homework LOL
