61eae8da5ec96797723f1ce0e3c7e3eb1c19694e
eddy duroseau <eddyduroseau@gmail.com>
2020-10-28 17:58:36 -0400

    10.26hw.js

---
+++ notes/10.26hw.js
@@ -0,0 +1,62 @@
+//  function sumNum(num1, num2){
+//     return num1 + num2
+
+// console.log(sumNum(12, 8))
+// function divisionRemainder(num1, num2){
+//     return num1 / num2
+
+// console.log(divisionRemainder(144, 11))
+//  function divisionRemainder(num1, num2)
+//     return num1 % num2
+// }
+//  console.log(divisionRemainder(144, 11))
+// const num1 = (144 / 11)
+// const num2 = (144 % 11)
+// function remainder(calculate){
+//     return caulculate 
+// }
+
+// console.log(num1, ('remainder') ,num2)
+// function randomNums(goFigure){
+    
+// }
+// console.log(Math.random() * 1000)
+
+// function theType(dataType){
+//     return dataType * dataType
+// }
+// console.log(theType(7, 5))
+
+// function isItArray(arrayChecker){
+//     return arrayChecker
+// }
+
+// console.log(Array.isArray(isItArray))
+
+// let sports = ['basketball', 'hockey', 'baseball', 'football', 'fencing', 'chess']
+// function fifthIndex(noSweat){
+//     return sports[5]
+// }
+// console.log(fifthIndex(sports))
+
+// let name = 'eddy duroseau' 
+// function myInitials(firstName, LastName){
+// console.log(name.charAt(0))
+// console.log(name.charAt(5))
+    
+//     return 'my Initials are ' 
+    
+// }
+// console.log(myInitials(name))
+
+let sports = ['basketball', 'hockey', 'baseball', 'football', 'fencing', 'chess']
+function 
+
+Sorry Teacher: I was stuck on number 8, but I will grab asession with one of you experts or just google instanceof, I am turning this in incomplete for now
+
+
+
+
+
+
+
