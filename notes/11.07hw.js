f03b65e1da8098b2809921161db0f5100c973724
eddy duroseau <eddyduroseau@gmail.com>
2020-11-09 00:07:34 -0500

    10.17hw.js

---
+++ notes/11.07hw.js
@@ -0,0 +1,56 @@
+// let school = 'nebula'
+// function topNotch(education){
+//     for (i = 0; i < education.length; i++){
+//         console.log(education[i])
+//     }
+//     return 'Done'
+// }
+// console.log(topNotch(school[0]))
+// console.log(topNotch(school[1]))
+// console.log(topNotch(school[2]))
+// console.log(topNotch(school[3]))
+// console.log(topNotch(school[4]))
+// console.log(topNotch(school[5]))
+
+// function speedLimit(){
+//     for (let i = 0; i <= 55; i++){
+//         console.log(i)
+//     }
+//         return "done"
+
+// }
+// console.log(speedLimit())=> 0 - 55 (consecutively) 'Done'
+   
+// let tools = ['wrench', 'hammer','screwdriver', 'saw', 'level']
+// function jobDone(work){
+//     for (i = 0; i < tools.length; i++ ){
+// console.log(work(i))
+//     }
+//     return 'Yup'
+// }
+// console.log(tools)=> [ 'wrench', 'hammer', 'screwdriver', 'saw', 'level' ]
+
+// let pastime ='writing thriller novels'
+// function hobby(writing){
+//     for (let i = 0; i <=pastime.length -1; i++){
+//         console.log(i)
+//     }
+//     return `Oh Snap`
+// }
+// console.log(hobby(pastime))= 0 -22 + 'Oh Snap'
+
+// function allEven(){
+//     for (i = 0; i <= 10; i+=2){
+// console.log(i)
+        
+//     }
+//     return 'Done' 
+// }
+// console.log(allEven())=> 0
+2
+4
+6
+8
+10
+Done
+
