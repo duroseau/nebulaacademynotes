21be6f265ff34e7a5279448f55a6f32a0e171b1c
eddy duroseau <eddyduroseau@gmail.com>
2020-11-09 15:20:30 -0500

    10.28hwfinal.js

---
+++ notes/10.28hwfinal.js
@@ -0,0 +1,137 @@
+// 0.28.20 More Function Review
+// Due October 31, 2020 9:00 AM
+// Instructions
+// Problem 1
+
+// Create a function called initials that has two parameters: firstName and lastName.
+
+
+
+// // This function will return the initials of the first and last names passed into the function
+
+// function initials (firstName, lastName = ''){
+   
+//     return firstName[0] + lastName[0]
+
+// }
+// console.log(initials('Eddy', 'Duroseau'))=> ED
+// Problem 2
+
+// Create a function called bool that has returns the Boolean of any argument passed into it*******
+// let untrue = (3, 7)
+// function bool(tact){
+
+//     return tact
+// }
+     
+
+//  console.log(bool(3===7))
+
+ 
+
+// Problem 3
+
+// Create a function called replaceA that takes in a string as a parameter.
+
+// This function will replace all a's in the string passed into the funciton with a '-'
+
+//   let health = 'a daily ritual to advance'
+//  function replaceA(sweat){
+   
+ 
+//      return sweat.replace(/a/g, '-')
+//  }
+//  console.log(replaceA(health))=> - d-ily ritu-l to -dv-nce
+ 
+
+// Problem 4
+
+// Create a function called characterAt that takes in a string as an argument
+
+// This function will return the character at the 5th index of any string passed into the function
+
+
+// function characterAt(index = ('')){
+//     return index[5]
+// }
+// console.log(characterAt(`i've been waiting for this moment`))
+ 
+
+// Problem 5
+
+// Create a function called squareRoot that takes in a number as a parameter.
+
+// This function will return the square roof of any number that is passed in as an argument
+// function squareRoot(p){
+//     return p * p
+// }
+// console.log(squareRoot(9))//=> 81
+
+ 
+
+// Problem 6
+
+// Create a function called theLength that takes in a string as a parameter.
+
+// This function will return the length of any string passed into this function
+
+//     function theLength(moolah){
+//     return moolah.length
+// }
+// console.log(theLength('the root of all evil'))//=> 20
+ 
+
+// Problem 7
+
+// Create a function called lastChar that takes in a string as a parameter.
+
+// This function will return the last character of any string passed in as an argument
+
+// function lastchar(enjoy){
+//     return enjoy[enjoy.length -1]
+// }
+// console.log(lastchar('life is what you make it'))//=> t
+ 
+
+// Problem 8
+
+// Create a function called itConcats that takes in 2 parameter that are arrays.
+
+// This function will combine the two arrays passed into this function and return the new combined array
+// let plants = ['cactus','aloe vera', 'fern', 'cyclamen']
+// let trees = ['palm', 'apple', 'cherry', 'pear', 'peach']
+// function itConcats(nature){
+//     console.log(plants)
+//     console.log(trees)
+//     return [nature] + (plants.concat(trees))
+// }
+// console.log(itConcats(plants))//=> 'cactus', 'aloe vera', 'fern', 'cyclamen' ]
+//  ['palm', 'apple', 'cherry', 'pear', 'peach' ]
+ 
+
+// Problem 9
+
+// Create a function called lastTwoEle that takes in an array as a parameter.
+
+// This function will return the last two elements of any array passed into this function as an argument.
+
+// function lastTwoEle(ihop){
+    
+//     return  ihop.slice([-2])
+// }
+//  console.log(lastTwoEle(['toast', 'eggs', 'juice', 'pancake', 'oatmeal']))//=> 'pancake', 'oatmeal']=> [ 'pancake', 'oatmeal' ]
+
+
+// Problem 10
+
+// Create a function called removeFirst that takes in an array as a parameter.
+
+// This function removes the first elements from any array that is passed into this function as an argument.
+
+// function removeFirst(notoast){
+//     return notoast.slice(1)
+// }
+// console.log(removeFirst(['toast', 'eggs', 'juice', 'pancake', 'oatmeal']))//=> 'eggs', 'juice', 'pancake', 'oatmeal'=> [ 'eggs', 'juice', 'pancake', 'oatmeal' ]
+
+// I think I almost got it
+
