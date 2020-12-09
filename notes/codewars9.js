b90c5b1c4e37e95564701b2ea5f8021f9eae99a3
eddy duroseau <eddyduroseau@gmail.com>
2020-12-09 16:53:34 -0500

    codewars9.js

--- notes/codewars9.js
+++ notes/codewars9.js
@@ -5,12 +5,17 @@ Quarter of the year
 
 https://www.codewars.com/kata/5ce9c1000bab0b001134f5af
 
+const quarterOf = (month) => {
+  let quarter = month/3;
+  return Math.ceil(quarter);
+}
  
 
 Multiplication Table
 
 https://www.codewars.com/kata/5a2fd38b55519ed98f0000ce
-
+if(n >0) {
+  return [n]
  
 
 Enough Space
@@ -41,3 +46,11 @@ https://www.codewars.com/kata/5808dcb8f0ed42ae34000031
 A Wolf in Sheep's Clothing
 
 https://www.codewars.com/kata/5c8bfa44b9d1192e1ebd3d15
+queue = queue.reverse();
+if (queue[0] === 'wolf') return "Pls go away and stop eating my sheep";
+for (let i = 0; i < queue.length; i++){
+  if (queue[i] === 'wolf') {
+    return `Oi! Sheep number ${i}! You are about to be eaten by a wolf!`
+  }
+}
+}
